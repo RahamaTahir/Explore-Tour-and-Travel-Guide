86444c67902e6b1348dd4df68d5a95ddcfb36efd
Rahama Tahir <rahmatahir5@gmail.com>
2018-11-17 11:24:28 +0500

    evalute in MVC

---
+++ ExploreTandT/Controllers/TourController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExploreTandT.Controllers
+{
+    public class TourController : Controller
+    {
+        // GET: Tour
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AllPackages()
+        {
+            return View();
+        }
+        public ActionResult AdminAccount()
+        {
+            return View();
+        }
+        public ActionResult AddPackage()
+        {
+            return View();
+        }
+        public ActionResult GetPackage()
+        {
+            return View();
+        }
+        public ActionResult PackageConfirmation()
+        {
+            return View();
+        }
+        public ActionResult RelatedPackages()
+        {
+            return View();
+        }
+        public ActionResult TermsandConditions()
+        {
+            return View();
+        }
+        public ActionResult TourGuideAccount()
+        {
+            return View();
+        }
+        public ActionResult TouristAccount()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExploreTandT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExploreTandT.Startup))]
+namespace ExploreTandT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
